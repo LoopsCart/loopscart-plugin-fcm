15a69685921c89f6c578a8709ac1234a8f29dde8
Aayush Kunwar <kunwar_aayush@live.com>
2025-08-18 12:38:48 +0545

    FIX: changed api prefix

--- fcm_messaging/urls.py
+++ fcm_messaging/urls.py
@@ -13,14 +13,13 @@ from .views import (
 )
 
 urlpatterns = [
-    path("fcm/certificate/", CertificateUploadView.as_view(), name="certificate-management"),
-    # path("fcm/certificate/", CertificateDetailView.as_view(), name="certificate-detail"),
-    path("fcm/devices/", GetUserDeviceView.as_view(), name="list-devices"),
-    path("fcm/device/", UserDeviceDetailView.as_view(), name="device-detail"),
-    path("fcm/group/", DeviceGroupView.as_view(), name="device-group-management"),
-    path("fcm/send-notification-group/", SendNotificationGroupView.as_view(), name="send-notification-to-group"),
-    path("fcm/send-notification-token/", SendNotificationToTokenView.as_view(), name="send-notification-to-token"),
-    path("fcm/send-notification-tokens/", SendNotificationToTokensView.as_view(), name="send-notification-to-tokens"),
-    path("fcm/send-notification-username/", SendNotificationToUsernameView.as_view(), name="send-notification-to-username"),
-    path("fcm/send-notification-usernames/", SendNotificationToUsernamesView.as_view(), name="send-notification-to-usernames"),
+    path("certificate/", CertificateUploadView.as_view(), name="certificate-management"),
+    path("devices/", GetUserDeviceView.as_view(), name="list-devices"),
+    path("device/", UserDeviceDetailView.as_view(), name="device-detail"),
+    path("group/", DeviceGroupView.as_view(), name="device-group-management"),
+    path("send-notification-group/", SendNotificationGroupView.as_view(), name="send-notification-to-group"),
+    path("send-notification-token/", SendNotificationToTokenView.as_view(), name="send-notification-to-token"),
+    path("send-notification-tokens/", SendNotificationToTokensView.as_view(), name="send-notification-to-tokens"),
+    path("send-notification-username/", SendNotificationToUsernameView.as_view(), name="send-notification-to-username"),
+    path("send-notification-usernames/", SendNotificationToUsernamesView.as_view(), name="send-notification-to-usernames"),
 ]
